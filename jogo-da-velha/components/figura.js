6552bb46e0b44f9feda4ffbc9580b868498375b7
NicolasRDuarte <drnicolasribeiroduarte@gmail.com>
2024-08-01 11:19:42 -0300

    Terminando o Projeto

--- jogo-da-velha/components/figura.js
+++ jogo-da-velha/components/figura.js
@@ -10,27 +10,7 @@ export default function Figura({ vetor, posicao, clicado }) {
         return "pencil";
     };
 
-    const winGame = (number) => {
-        if (
-            // Linhas iguais
-            (array[0] === array[number] && array[1] === array[number] && array[2] === array[number])
-            || (array[0] === array[number] && array[1] === array[number] && array[2] === array[number])
-            || (array[0] === array[number] && array[1] === array[number] && array[2] === array[number])
-
-            // Colunas iguais
-            || (array[0] === array[number] && array[3] === array[number] && array[6] === array[number])
-            || (array[1] === array[number] && array[4] === array[number] && array[7] === array[number])
-            || (array[2] === array[number] && array[5] === array[number] && array[8] === array[number])
-            
-            // Diagonais iguais
-            || (array[0] === array[number] && array[4] === array[number] && array[8] === array[number])
-            || (array[2] === array[number] && array[4] === array[number] && array[6] === array[number])
-            
-        ){
-
-        }
-    }
-
+  
     return (
         <TouchableOpacity onPress={clicado}>
             <Entypo name={getIcon()} size={80} color="#000" />
